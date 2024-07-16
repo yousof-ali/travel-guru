0a069c009dc77dbc43f2b19fc2622e5cbb98ac32
Md Mahbub Alom <mdmahbuba034@gmail.com>
2024-07-17 01:35:44 +0600

    hide icon

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -2,49 +2,54 @@ import { Link, useNavigate } from "react-router-dom";
 import { useContext, useState } from "react";
 import { authProvider } from "../../AuthProvider/AuthProvider";
 import { updateProfile } from "firebase/auth";
-
+import { BiSolidHide } from "react-icons/bi";
+import { IoEye } from "react-icons/io5";
 
 const Register = () => {
-    const {createUser}=useContext(authProvider);
-    const [error,setError]=useState("");
-    const navi = useNavigate();
+  const { createUser } = useContext(authProvider);
+  const [error, setError] = useState("");
+  const navi = useNavigate();
+  const [hide, setHide] = useState(false);
 
-    const handleRegister = e =>{
-        e.preventDefault();
-        setError("");
-        const userName = e.target.name.value;
-        const userImgUrl = e.target.photo.value;
-        const userEmail = e.target.email.value;
-        const userPassword = e.target.password.value;
-        console.log(userName,userEmail,userImgUrl,userPassword)
+  const handleRegister = (e) => {
+    e.preventDefault();
+    setError("");
+    const userName = e.target.name.value;
+    const userImgUrl = e.target.photo.value;
+    const userEmail = e.target.email.value;
+    const userPassword = e.target.password.value;
+    console.log(userName, userEmail, userImgUrl, userPassword);
 
-        createUser(userEmail,userPassword)
-        .then((result)=>{
-          console.log(result.user)
-          updateProfile(result.user,{
-            displayName:userName,
-            photoURL:userImgUrl
-          })  
-          .then(()=>{
-            navi('/')
+    createUser(userEmail, userPassword)
+      .then((result) => {
+        console.log(result.user);
+        updateProfile(result.user, {
+          displayName: userName,
+          photoURL: userImgUrl,
+        })
+          .then(() => {
+            navi("/");
           })
-          .catch((error)=>{
-            setError(error.message)
-          })         
-        })
-        .catch((error)=>{
-          setError(error.message);
-        })
-        
-    }
+          .catch((error) => {
+            setError(error.message);
+          });
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
+  const handleHide = () => {
+    setHide(!hide);
+  };
 
   return (
     <div>
-      <div className="hero py-24  bg-base-200 min-h-screen ">
+      <div className="hero py-24   min-h-screen ">
         <div className="hero-content">
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
             <div className="card-body">
-              <form onSubmit={handleRegister} >
+              <form onSubmit={handleRegister}>
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Your Name</span>
@@ -78,32 +83,43 @@ const Register = () => {
                     placeholder="photo url"
                     className="input input-bordered"
                     name="photo"
-                    
                   />
                 </div>
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Password</span>
                   </label>
-                  <input
-                    type="password"
-                    placeholder="password"
-                    name="password"
-                    className="input input-bordered"
-                    required
-                  />
+                  <div className="flex items-center gap-2">
+                    <input
+                      type={hide ? "text" : "password"}
+                      placeholder="password"
+                      name="password"
+                      className="input input-bordered"
+                      required
+                    />
+                    <div className="text-2xl" onClick={handleHide}>
+                      {
+                        hide?<BiSolidHide />
+                        :<IoEye/>
+                      }
+                    
                   
+                    </div>
+                  </div>
                 </div>
-                {
-                  error&&<p className="text-red-600 text-sm">{error}</p>
-                }
+                {error && <p className="text-red-600 text-sm">{error}</p>}
                 <div className="form-control mt-6">
                   <button className="btn bg-orange-600 hover:bg-orange-300 text-white">
                     Register
                   </button>
                 </div>
               </form>
-              <p>Already register? <Link to={"/login"} className="btn btn-link">login</Link> </p>
+              <p>
+                Already register?{" "}
+                <Link to={"/login"} className="btn btn-link">
+                  login
+                </Link>{" "}
+              </p>
             </div>
           </div>
         </div>
--- src/Pages/login/Login.jsx
+++ src/Pages/login/Login.jsx
@@ -1,51 +1,56 @@
 import { useContext, useRef, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { authProvider } from "../../AuthProvider/AuthProvider";
+import { BiSolidHide } from "react-icons/bi";
+import { IoEye } from "react-icons/io5";
+
+
 const Login = () => {
-  const {login,resetPassword}=useContext(authProvider)
-  const[sucess,setSucess]=useState("");
-  const[error,setError]=useState("");
+  const { login, resetPassword } = useContext(authProvider);
+  const [sucess, setSucess] = useState("");
+  const [error, setError] = useState("");
   const emailref = useRef();
-  const navigate = useNavigate()
-  const location=useLocation()
-  console.log(location)
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [hide,setHide]=useState(false);
+  console.log(location);
 
   const handleLogin = (e) => {
     e.preventDefault();
-    setError("")
-    const userEmail = e.target.email.value ;
+    setError("");
+    const userEmail = e.target.email.value;
     const userPassword = e.target.password.value;
 
-    login(userEmail,userPassword)
-    .then(()=>{
-      console.log("successfully login")
-      navigate(location?.state?location.state:'/')
-      
-    })
-    .catch((error)=>{
-      setError(error.message)
-    })
+    login(userEmail, userPassword)
+      .then(() => {
+        console.log("successfully login");
+        navigate(location?.state ? location.state : "/");
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
   };
 
-  const handleForGetPassword = ()=>{
+  const handleForGetPassword = () => {
     setSucess("");
-    setError('');
-    const userEmail = emailref.current.value
-    if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(userEmail)){
-      setError("type a valid email!")
+    setError("");
+    const userEmail = emailref.current.value;
+    if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(userEmail)) {
+      setError("type a valid email!");
       return;
     }
-    resetPassword(userEmail)
-    .then(()=>{
-      setSucess("check your mail "+ userEmail)
-    })
+    resetPassword(userEmail).then(() => {
+      setSucess("check your mail " + userEmail);
+    });
+  };
 
+  const handleHide=()=>{
+    setHide(!hide);
   }
   return (
     <div>
-      <div className="hero bg-base-200 min-h-screen ">
+      <div className="hero min-h-screen ">
         <div className="hero-content">
-            
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
             <div className="card-body">
               <form onSubmit={handleLogin}>
@@ -66,27 +71,51 @@ const Login = () => {
                   <label className="label">
                     <span className="label-text">Password</span>
                   </label>
-                  <input
-                    type="password"
-                    placeholder="password"
-                    name="password"
-                    className="input input-bordered"
-                    required
-                  />
+                  <div className="flex items-center gap-2">
+                    <input
+                      type={hide?"text":"password"}
+                      placeholder="password"
+                      name="password"
+                      className="input input-bordered"
+                      required
+                    />
+                    <div className="text-2xl" onClick={handleHide}>
+                      {
+                        hide?<BiSolidHide />
+                        :<IoEye/>
+                      }
+                    
+                  
+                    </div>
+                  </div>
                   <label className="label">
-                    <a onClick={handleForGetPassword} href="#" className="label-text-alt link link-hover">
+                    <a
+                      onClick={handleForGetPassword}
+                      href="#"
+                      className="label-text-alt link link-hover"
+                    >
                       Forgot password?
                     </a>
                   </label>
                 </div>
-                {
-                  error?<p className="text-sm text-red-600">{error}</p>:<p className="text-sm text-green-600">{sucess}</p>
-                }
+                {error ? (
+                  <p className="text-sm text-red-600">{error}</p>
+                ) : (
+                  <p className="text-sm text-green-600">{sucess}</p>
+                )}
                 <div className="form-control mt-6">
-                  <button className="btn bg-orange-600 hover:bg-orange-300 text-white">Login</button>
+                  <button className="btn bg-orange-600 hover:bg-orange-300 text-white">
+                    Login
+                  </button>
                 </div>
               </form>
-              <p> No account? <Link to={"/register"} className="btn btn-link">register</Link> </p>
+              <p>
+                {" "}
+                No account?{" "}
+                <Link to={"/register"} className="btn btn-link">
+                  register
+                </Link>{" "}
+              </p>
             </div>
           </div>
         </div>
